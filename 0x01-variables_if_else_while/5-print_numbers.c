85e798adc5c49e0767a747f4e9f30ca071a0460c
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-09-10 07:19:40 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the single numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+    int i = 0;
+
+    while (i < 10)
+    {
+        printf("%i", i);
+        i++;
+    }
+    printf("\n");
+
+    return (0);
+}

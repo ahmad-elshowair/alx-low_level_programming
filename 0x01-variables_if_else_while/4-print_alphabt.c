acbaf03c7c81c5c52bb4122ac4cfee2ff77f7699
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-09-10 07:10:09 -0700

    4. When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all Alphabets but q and e.
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while(c <= 'z')
+	{
+		if(c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}

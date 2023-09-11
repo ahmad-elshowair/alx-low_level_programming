d8d74b93a344be3fa703d01dfa1e9b31103e5c15
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-09-10 17:31:20 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char hex[] = "0123456789abcdef";
+	int i = 0;
+
+	while (i < 16)
+	{
+		putchar(hex[i]);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}

eee9180a279e27fbc5104983e0b597387afeb36d
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-09-12 04:37:41 -0700

    0. _putchar 0.2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
 
 /**
  * main - prints _putchar, followed by a new line
@@ -8,7 +7,15 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('e');
+	_putchar('\n');
 
 	return (0);
 }

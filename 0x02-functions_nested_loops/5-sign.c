868d5dd2bbe88a8a3420b06aa5fe156020ed1de1
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-09-13 16:57:57 -0700

    5. Sign 0.2

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,29 +9,24 @@
 int print_sign(int n)
 {
 	int r;
+
 	if (n > 0)
 	{
-		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
 		_putchar(n);
 		r = 1;
 	}
+
 	else if (n == 0)
 	{
-		_putchar('0');
-                _putchar(',');
-                _putchar(' ');
                 _putchar(n);
                 r = 0;
 	}
+
 	else if (n < 0)
 	{
-		_putchar('-');
-                _putchar(',');
-                _putchar(' ');
                 _putchar(n);
                 r = -1;
 	}
+	
 	return (r);
 }

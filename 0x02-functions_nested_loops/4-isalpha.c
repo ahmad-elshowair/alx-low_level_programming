89969e556e7e0dfc27b15e4b468e4714fd6e72c6
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-09-13 16:25:42 -0700

    4. isalha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isalpha - check if the input is alaphabet
+ * @c - it the checked character
+ * Return: 1 if c is a letter, or 0 if not
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c))
+		return (1);
+	else
+		return (0);
+
+}
---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - checks for alphabetic character.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	chart c = '3';
+
+	_isalpha(c);
+
+	return (0);
+}

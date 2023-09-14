a20b9d5b825778534abfd0eaf5137f4dafca185f
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-09-14 07:28:38 -0700

    11. 98 Battery Street, the OG 0.1

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - print all numbers to 98
@@ -6,8 +7,34 @@
  */
 void print_to_98(int n)
 {
-	while (n <= 98)
+	int i, j;
+
+	if (n <= 98)
 	{
-		_putchar(n + '0');
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				printf("%i ", i)
+			}
+			else if (i == 98)
+			{
+				printf("%i\n", i); 
+			}
+		}
+	}
+	else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			 if (j != 98)
+                        {
+                                printf("%i ", j)
+                        }
+                        else if (j == 98)
+                        {
+                                printf("%i\n", j;
+                        }
+		}
 	}
 }

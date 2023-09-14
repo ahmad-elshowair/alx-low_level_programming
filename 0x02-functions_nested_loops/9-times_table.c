b285e3a4ad0cb18519f19e3ee660b7a7210f734e
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-09-14 06:52:55 -0700

    9. Learn your times table 0.4

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,11 +12,26 @@ void times_table(void)
 		for (c = 0; c < 10; c++)
 		{
 			d = r * c;
-			_putchar(d + '0');
-			if (c < 9)
+			
+			if (c == 0)
+			{
+				_putchar(d + '0');
+			}
+
+			if (d < 10 && c != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(d + '0');
+			}
+
+			else if (d >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((d / 10) + '0');
+				_putchar((d % 10) + '0');
 			}
 		}
 		_putchar('\n');

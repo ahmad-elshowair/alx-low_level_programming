2cec70bb97aeeef709413e4771daee27de370c8e
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-09-14 07:04:25 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers.
+ * @n1: the first integer
+ * @n2: the second integer
+ * Return: the result.
+ */
+int add(int n1, int n2)
+{
+	int r;
+
+	r = n1 + n2;
+
+	return (r);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - adds two integers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n1 = 3, n2 = 4;
+
+	add(n1, n2);
+
+	return (0);
+}

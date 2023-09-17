899fbe4d922abe596418fbcc95d8f14ff0d2c7ef
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-09-16 21:32:26 -0700

    2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - call in the mul function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%d\n", mul(434, 65));
+	printf("%d\n", mul(-343, 45));
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - mulitplies two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the result of multiplies two integers
+ */
+int mul(int a, int b)
+{
+	int r = a * b;
+
+	return (r);
+}

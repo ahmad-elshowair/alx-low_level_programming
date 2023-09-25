5dcf6bdda00a3f18e5d020757b4971a1000ee0f1
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-09-25 04:26:16 -0700

    fix the spacing in task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,14 +2,16 @@
 #include <string.h>
 
 /**
- * print_rev - a function that prints a string, in reverse, followed by a new line.
+ * print_rev - a function that prints a string, in reverse,
+ * followed by a new line.
  * @string: it is a pointer to the string
  */
 void print_rev(char *string)
 {
-    int i, length;
-    length = strlen(string);
-    for (i = length - 1; i >= 0 && *(string + i) != '\n'; --i)
-        _putchar(*(string + i));
-    _putchar('\n');
+	int i, length;
+
+	length = strlen(string);
+	for (i = length - 1; i >= 0 && *(string + i) != '\n'; --i)
+		_putchar(*(string + i));
+	_putchar('\n');
 }

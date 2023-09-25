46f6ff3ca29e9fdd21e161c5773b261af08d8a8a
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-09-25 08:35:02 -0700

    task 7 print half a string 0.4

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,20 +9,18 @@ void puts_half(char *str)
 {
 	int length, i, half_length;
 
-	 length = strlen(str);
-	 if (length % 2 == 0)
-	 {
-		 half_length = length / 2;
-	 }
-	 else
-	 {
-		 half_length = (length + 1) / 2;
-	 }
-	 for (i = half_length; i < length; i++)
-	 {
-		 _putchar(str[i]);
-	 }
-	 _putchar('\n');
-
-
+	length = strlen(str);
+	if (length % 2 == 0)
+	{
+		half_length = length / 2;
+	}
+	else
+	{
+		half_length = (length + 1) / 2;
+	}
+	for (i = half_length; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }

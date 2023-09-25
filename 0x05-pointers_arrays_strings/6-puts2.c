515bb418f77f33566cad574844e791eb4e5b8adb
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-09-25 06:56:39 -0700

    task 6 print the other character 0.2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,7 +5,7 @@
  * starting with the first character, followed by a new line.
  * @str: it is an argument of char
  */
-void puts2(char *st)
+void puts2(char *str)
 {
 	int i;
 	for (i = 0; i < _strlen(str); i += 2)

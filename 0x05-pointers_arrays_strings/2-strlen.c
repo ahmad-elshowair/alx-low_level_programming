d0f8ae7393e79f5a1595825fb62622fbf8ae2856
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-09-24 22:53:48 -0700

    2. This report, by its very length, defends itself against the risk of being read 0.2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
 
 /**
  * _strlen - a function that returns the length of a string
- * @s: it is a pointer 
+ * @s: it is a pointer
  * Return: return the length of a string.
  */
 int _strlen(char *s)

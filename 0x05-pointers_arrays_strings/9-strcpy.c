6014b9f0182bbee7fb6b01469443e99309220f06
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-09-25 09:43:07 -0700

    task 9 copy string of a pointer to another pointer 0.0

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - a function that copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest.
+ * @dest: a pointer of the destination
+ * @src: a pointer of the source string
+ * Return: a char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return strcpy(dest, src);
+}

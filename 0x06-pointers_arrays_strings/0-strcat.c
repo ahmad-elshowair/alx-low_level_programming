c03aa86e90be3c7df520851343043d0f03a32780
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-09-27 05:33:11 -0700

    0. strcat 0.0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcat - a function that concatenates two strings.
+ * @dest: it the first string.
+ * @src: it is the second string.
+ * Return: return one concatenating string.
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
